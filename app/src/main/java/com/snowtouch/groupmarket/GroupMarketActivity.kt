68a850a3776342db7b6570fa01189a0597505340
Snowtouch <138367829+Snowtouch@users.noreply.github.com>
2024-01-16 22:00:22 +0100

    Initial commit

---
+++ app/src/main/java/com/snowtouch/groupmarket/GroupMarketActivity.kt
@@ -0,0 +1,18 @@
+package com.snowtouch.groupmarket
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.snowtouch.groupmarket.ui.theme.GroupMarketTheme
+
+class GroupMarketActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { GroupMarketApp() }
+    }
+}
---
+++ app/src/main/java/com/snowtouch/groupmarket/GroupMarketApp.kt
@@ -0,0 +1,5 @@
+package com.snowtouch.groupmarket
+
+class GroupMarketApp {
+
+}
